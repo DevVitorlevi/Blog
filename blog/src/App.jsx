b96093ee96d901a4bd924db0407a7db4b7668699
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-15 17:00:41 -0300

    Adicionando O React Router Dom

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Router } from 'react-router-dom'
 import GlobalStyles from './Styles/Global'
 
 function App() {
@@ -6,6 +6,15 @@ function App() {
   return (
     <>
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+
+          <Router path='/' elements={<Home />} />
+          <Router path='/about' elements={<About />} />
+          <Router path='*' elements={<NotFound />} />
+
+        </Routes>
+      </BrowserRouter>
       <h1>Blog</h1>
     </>
   )
