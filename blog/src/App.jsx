5cd5077a53e07fd6998240ec16a89564b549dc96
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-23 08:57:59 -0300

    context de auth

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -8,22 +8,25 @@ import Login from './pages/Login'
 import NotFound from './pages/NotFound'
 //components
 import Header from './Components/Header'
+import { AuthProvider } from './Context/AuthContext'
 
 function App() {
 
   return (
     <>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' elements={<Home />} />
-          <Route path='/about' elements={<About />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' elements={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' elements={<Home />} />
+            <Route path='/about' elements={<About />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' elements={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
